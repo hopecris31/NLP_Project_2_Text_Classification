d0267cd5366d332e03f4b08c9bcdabe6a942a194
hopecris31 <hope.crisafi@gmail.com>
2023-01-30 00:17:26 -0500

    Transferred to PC

--- complex_word_classification_tests.py
+++ complex_word_classification_tests.py
@@ -1,5 +1,6 @@
 import testing as t
 import complex_word_classification as c
+#hi
 
 dataMAC = "/Users/hopecrisafi/Desktop/Natural Language Processing/data"
 dataPC = "C:/Users/hopec/OneDrive/Desktop/data/complex_words_test_unlabeled.txt"
